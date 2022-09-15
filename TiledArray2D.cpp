3aea7372f42c0f92c202be96871f7d0b107d44f8
Extra-Creativity <72960952+Extra-Creativity@users.noreply.github.com>
2022-09-15 20:36:16 +0800

    Test for random permutation.

--- TiledArray2D.cpp
+++ TiledArray2D.cpp
@@ -1,4 +1,5 @@
 // Experiments on tailed 2D matrix.
+// If in a tailed and normally sequenced way : 
 // Clang 15.0.0 -O3 output : 
 /* Empty loop : 9.8e-08s
    Normal matrix : 0.000580095s
@@ -15,12 +16,37 @@
    Padding, lookup matrix : 0.000834393s
    Fill in right.
 */
-// MSVC 19.32 /O2 /Oi output : 
+// x64 MSVC 19.32 /O2 /Oi output : 
 /* Empty loop : 1e-07s
    Normal matrix : 0.0014819s
    No padding, no lookup matrix : 0.0020457s
    Padding, no lookup matrix : 0.0016727s
    Padding, lookup matrix : 0.0014371s
+*/
+// If in a random permutation way :
+// Clang 15.0.0 -O3 output : 
+/* Empty loop : 1.2e-07s
+   Normal matrix : 0.000967834s
+   No padding, no lookup matrix : 0.0009548s
+   Padding, no lookup matrix : 0.000940871s
+   Padding, lookup matrix : 0.00115011s
+   Fill in right.
+*/
+// GCC 12.2 -O3 output :
+/* Empty loop : 1.1e-07s
+   Normal matrix : 0.000880904s
+   No padding, no lookup matrix : 0.000898521s
+   Padding, no lookup matrix : 0.000930923s
+   Padding, lookup matrix : 0.00121801s
+   Fill in right.
+*/
+// x64 MSVC 19.32 /O2 /Oi output : 
+/* Empty loop : 1e-07s
+   Normal matrix : 0.0017142s
+   No padding, no lookup matrix : 0.0023464s
+   Padding, no lookup matrix : 0.0019774s
+   Padding, lookup matrix : 0.001578s
+   Fill in right.
 */
 // Unfortunately, there seems to be little acceleration.
 
@@ -155,6 +181,7 @@ private:
     std::vector<T> m_arr = std::vector<T>(rowNum * colNum);
 };
 
+// main1.cpp, normally sequenced.
 constexpr std::array matShape{ 1024,1024 };
 constexpr std::array tileShape{ 16, 16 };
 const int checkTimes = 100;
@@ -255,6 +282,163 @@ int main()
                     if (j + jj >= matShape[1]) [[unlikely]]
                         break;
                     arrPL(i + ii, j + jj) = ++cnt;
+                }
+            }
+    endTime = std::chrono::steady_clock::now();
+    std::cout << std::format("Padding, lookup matrix : {}\n", GetIntervalSecond(beginTime, endTime));
+
+    // Randomly check value.
+    std::random_device rd{};
+    std::uniform_int_distribution<int> distribution1(0, matShape[0] - 1),
+        distribution2(0, matShape[1] - 1);
+    std::default_random_engine generator1{ rd() }, generator2{rd()};
+    int i = 0, j = 0;
+    bool success = true;
+    for (int _ = 0; _ < checkTimes; _++)
+    {
+        i = distribution1(generator1), j = distribution2(generator2);
+        auto temp1 = normalMatrix(i, j), temp2 = arrNPNL(i, j),
+            temp3 = arrPNL(i, j), temp4 = arrPL(i, j);
+        if (!((temp1 == temp2) && (temp2 == temp3) && (temp3 == temp4)))
+        {
+            success = false;
+            break;
+        }
+    }
+    if (success)
+        std::cout << "Fill in right.\n";
+    else
+        std::cout << std::format("Oops, wrong in ({}, {})\n", i, j);
+
+    return 0;
+}
+
+// main2.cpp, random permutation
+constexpr std::array matShape{ 1024,1024 };
+constexpr std::array tileShape{ 16, 16 };
+const int checkTimes = 100;
+std::array<std::array<std::pair<size_t, size_t>, tileShape[1]>, tileShape[0]> sequenceInBlock;
+
+void GenerateRandomPermutation()
+{
+    const int tileSize = tileShape[0] * tileShape[1];
+    int cnt = tileSize;
+    std::random_device rd{};
+    std::uniform_int_distribution<int> distribution(0, tileSize - 1);
+    std::default_random_engine generator{ rd() };
+    while (cnt)
+    {
+        int randVal1 = distribution(generator);
+        if (randVal1 / cnt > tileSize / cnt)
+            continue;
+        int id1 = randVal1 % cnt;
+
+        int randVal2 = distribution(generator);
+        if (randVal2 / cnt > tileSize / cnt)
+            continue;
+        int id2 = randVal2 % cnt;
+
+        --cnt;
+        sequenceInBlock[cnt / tileShape[0]][cnt % tileShape[0]] = { id1,id2 };
+    }
+    return;
+}
+
+int main()
+{
+    GenerateRandomPermutation();
+    // generate a 1024 * 1024 matrix.
+    VectorWrapper<int, matShape[0], matShape[1]> normalMatrix;
+
+    // For no padding & no lookup version.
+    TailedArray2D<int, matShape[0], matShape[1], tileShape[0], tileShape[1], false, false> arrNPNL;
+
+    // For padding & no loopup version.
+    TailedArray2D<int, matShape[0], matShape[1], tileShape[0], tileShape[1], false, true> arrPNL;
+
+    // For padding & lookup version
+    TailedArray2D<int, matShape[0], matShape[1], tileShape[0], tileShape[1], true, true> arrPL;
+
+    int cnt = 0;
+    auto beginTime = std::chrono::steady_clock::now(), endTime = beginTime;
+    for (int i = 0; i < matShape[0]; i += tileShape[0])
+        for (int j = 0; j < matShape[1]; j += tileShape[1])
+            for (int ii = 0; ii < tileShape[0]; ii++)
+            {
+                for (int jj = 0; jj < tileShape[1]; jj++)
+                {
+                    int row = i + sequenceInBlock[ii][jj].first, col = j + sequenceInBlock[ii][jj].second;
+                    if (row > matShape[0] || col > matShape[1])
+                        continue;
+                    ++cnt;
+                }
+            }
+    endTime = std::chrono::steady_clock::now();
+    std::cout << std::format("Empty loop: {}\n", GetIntervalSecond(beginTime, endTime));
+
+    cnt = 0;
+    beginTime = std::chrono::steady_clock::now();
+    for (int i = 0; i < matShape[0]; i += tileShape[0])
+        for (int j = 0; j < matShape[1]; j += tileShape[1])
+            for (int ii = 0; ii < tileShape[0]; ii++)
+            {
+                for (int jj = 0; jj < tileShape[1]; jj++)
+                {
+                    int row = i + sequenceInBlock[ii][jj].first, col = j + sequenceInBlock[ii][jj].second;
+                    if (row > matShape[0] || col > matShape[1])
+                        continue;
+                    normalMatrix(i, j) = ++cnt;
+                }
+            }
+    endTime = std::chrono::steady_clock::now();
+    std::cout << std::format("Normal matrix : {}\n", GetIntervalSecond(beginTime, endTime));
+
+    cnt = 0;
+    beginTime = std::chrono::steady_clock::now();
+    for (int i = 0; i < matShape[0]; i += tileShape[0])
+        for (int j = 0; j < matShape[1]; j += tileShape[1])
+            for (int ii = 0; ii < tileShape[0]; ii++)
+            {
+                for (int jj = 0; jj < tileShape[1]; jj++)
+                {
+                    int row = i + sequenceInBlock[ii][jj].first, col = j + sequenceInBlock[ii][jj].second;
+                    if (row > matShape[0] || col > matShape[1])
+                        continue;
+                    arrNPNL(i, j) = ++cnt;
+                }
+            }
+    endTime = std::chrono::steady_clock::now();
+    std::cout << std::format("No padding, no lookup matrix : {}\n", GetIntervalSecond(beginTime, endTime));
+
+    cnt = 0;
+    beginTime = std::chrono::steady_clock::now();
+    for (int i = 0; i < matShape[0]; i += tileShape[0])
+        for (int j = 0; j < matShape[1]; j += tileShape[1])
+            for (int ii = 0; ii < tileShape[0]; ii++)
+            {
+                for (int jj = 0; jj < tileShape[1]; jj++)
+                {
+                    int row = i + sequenceInBlock[ii][jj].first, col = j + sequenceInBlock[ii][jj].second;
+                    if (row > matShape[0] || col > matShape[1])
+                        continue;
+                    arrPNL(i, j) = ++cnt;
+                }
+            }
+    endTime = std::chrono::steady_clock::now();
+    std::cout << std::format("Padding, no lookup matrix : {}\n", GetIntervalSecond(beginTime, endTime));
+
+    cnt = 0;
+    beginTime = std::chrono::steady_clock::now();
+    for (int i = 0; i < matShape[0]; i += tileShape[0])
+        for (int j = 0; j < matShape[1]; j += tileShape[1])
+            for (int ii = 0; ii < tileShape[0]; ii++)
+            {
+                for (int jj = 0; jj < tileShape[1]; jj++)
+                {
+                    int row = i + sequenceInBlock[ii][jj].first, col = j + sequenceInBlock[ii][jj].second;
+                    if (row > matShape[0] || col > matShape[1])
+                        continue;
+                    arrPL(i, j) = ++cnt;
                 }
             }
     endTime = std::chrono::steady_clock::now();
